531ee2b399670a3fd4594fe34f57081c2f0e7848
stanleyyork <stanleyyork@gmail.com>
2015-11-08 22:39:47 -0800

    added some light design

--- main.js
+++ main.js
@@ -5,14 +5,17 @@ $(function() {
 	var playerTwo = '';
 	var moveA = 0;
 	var moveB = 0;
-	var triviaLocations = {100: false, 400: false, 600: false, 900: false, 1000: false};
+	var triviaLocations = {100: false, 400: false, 600: false, 900: false};
 
-	$('#startGame').on('click', function(){
-		playerOne = prompt("What is player One's name?");
-		playerTwo = prompt("What is player Two's name?");
-		started = true;
+	$('#rules').click(function(){
+		alert("1. Hit 'A' (player one) or 'L' (player two) as fast as you can to move character. 2. Race to the end. 3. Answer trivia for extra jumps.")
 	});
 
+	$('#startGame').click(function(){
+		started = true;
+		playerOne = prompt("What is player One's name?");
+		playerTwo = prompt("What is player Two's name?");
+	});
 
 	$(document).keyup(function(e) {
 		if(started === true) {
@@ -30,21 +33,27 @@ $(function() {
 
 	function moveChar(char, moveValue){
 		if(char === "charA"){
+			// Grab move value and move character
 	  		moveA += moveValue;
 	  		$('#charA').css('left', moveA);
+	  		// Check if in trivia position and not already taken, if so, ask trivia
 	  		if((triviaPosition("charA", moveA) === true) && (triviaLocations[moveA] === false)){
 				triviaLocations[moveA] = true;
 				askTriviaQuestion("charA", playerOne, moveA);
 	  		}
-	  		//getWinner("charA");
+	  		// Check for winner
+	  		moveA > 750 ? getWinner("charA") : null;
   		} else if(char === "charB") {
+  			// Grab move value and move character
   		  	moveB += moveValue;
   			$('#charB').css('left', moveB);
+  			// Check if in trivia position and not already taken, if so, ask trivia
   			if((triviaPosition("charB", moveB) === true) && (triviaLocations[moveB] === false)){
 				triviaLocations[moveB] = true;
 				askTriviaQuestion("charB", playerTwo, moveB);
 	  		}
-	  		//getWinner("charB");
+	  		// Check for winner
+	  		moveB > 750 ? getWinner("charB") : null;
   		}
 	}
 
@@ -58,7 +67,6 @@ $(function() {
 		  case 400: return true;
 		  case 600: return true;
 		  case 900: return true;
-		  case 1000: return true;
 		  default: //console.log("Not on spot");
 		    break;
 		}
@@ -70,10 +78,10 @@ $(function() {
 			var answer = data[0].answer;
 			var category = data[0].category.title;
 			var triviaValue = data[0].value;
-			var location = findCharPosition(char);
 			console.log(answer);
 			var response = prompt(playerName + ": " + question + " (For " + triviaValue/2 + " jump points; Hint: the category is " + category + ").");
 			
+			// Ask question, receive response and determine outcome
 			if(response.toLowerCase() === answer.toLowerCase()){
 				alert("Yes! You get to move " + triviaValue/2 + " spots ahead!");
 				moveChar(char, triviaValue/2);
@@ -85,7 +93,8 @@ $(function() {
 	}
 
 	function getWinner(char){
-		if(findCharPosition(char) >= "1100px"){
+		if(findCharPosition(char) >= "1000px"){
+			console.log(findCharPosition(char));
 			alert("Congratulations " + char + ", you won!");
 		}
 	}
