358b721d951a2d11985d5d509ca164cebd8a2914
stanleyyork <stanleyyork@gmail.com>
2015-11-08 19:37:56 -0800

    figured out how to move characters across screen

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
 $(function() {
 
+	var moveA = 0;
+	var moveB = 0;
+	var movepixelsA = '';
+
+	$(document).keyup(function(e) {
+  		if(e.keyCode === 65){
+ 			moveChar("charA");
+  		} else if (e.keyCode === 76) {
+  			moveChar("charB");
+  		}
+	});
+
+	function moveChar(name){
+		if(name === "charA"){
+	  		moveA += 10;
+	  		$('#charA').css('left', moveA);
+  		} else if(name === "charB") {
+  		  	moveB += 10;
+  			$('#charB').css('left', moveB);
+  		}
+	}
+
 
 });
